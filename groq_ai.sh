6461bc7b13c85b2cac73cab9df50bff74c10ff82
gevorgalaverdyan <gevorgalaverdyanmail@gmail.com>
2024-11-11 13:07:42 -0500

    added page, manual input option, themes

---
+++ groq_ai.sh
@@ -0,0 +1,10 @@
+curl https://api.groq.com/openai/v1/chat/completions -s \
+-H "Content-Type: application/json" \
+-H "Authorization: Bearer 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" \
+-d '{
+"model": "llama3-8b-8192",
+"messages": [{
+    "role": "user",
+    "content": "Summerize this text: Artificial intelligence (AI) has become a transformative force in healthcare, promising to revolutionize everything from diagnostics and treatment planning to patient management and administrative tasks. The potential benefits of AI in healthcare are vast, as it can enhance accuracy, efficiency, and personalization of care, making healthcare systems more effective and patient-centered. Key applications include AI-powered imaging systems that help detect diseases earlier, predictive models that improve patient outcomes, and personalized treatment plans informed by data.One significant area of impact is medical imaging. AI algorithms have shown remarkable success in detecting abnormalities in X-rays, MRIs, and CT scans. By identifying patterns and anomalies that may be challenging for the human eye, AI can assist radiologists in making faster and more accurate diagnoses, particularly for conditions like cancer, fractures, and neurological disorders. This reduces the likelihood of misdiagnoses and allows for earlier intervention, potentially saving lives.Predictive analytics in patient care is another promising application. Using data from electronic health records (EHRs) and other sources, AI can identify patients at high risk for certain conditions, such as heart disease or diabetes, and suggest preventive measures. AI-driven risk assessment tools also help hospitals reduce readmission rates by pinpointing patients who may need additional support post-discharge, ensuring a higher quality of care. "
+}]
+}'
